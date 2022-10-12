3c755259ca78aa73f93911875612381d714723bf
Eduardo Nonemacher <ernonemacher@outlook.com>
2022-10-12 07:55:33 -0300

    Starta setup para integração com Unity

---
+++ lib/ar_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArPage extends StatefulWidget {
+  const ArPage({super.key});
+
+  @override
+  State<ArPage> createState() => _ArPageState();
+}
+
+class _ArPageState extends State<ArPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:arfudy_flutter/ar_page.dart';
+import 'package:arfudy_flutter/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,52 +16,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Arfudy"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        'ar_page': (context) => const ArPage(),
+      },
     );
   }
 }
