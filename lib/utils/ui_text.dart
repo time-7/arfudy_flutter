6c03ac9bf6d1919c092c4f0c0193351411ccfc33
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-12-02 10:22:09 -0300

    creating base ui classes

---
+++ lib/utils/ui_text.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'ui_scale.dart';
+
+class UIText extends StatelessWidget {
+  const UIText(this.text, {Key? key, this.fontSize, this.fontColor = Colors.black}) : super(key: key);
+  final String text;
+  final double? fontSize;
+  final Color fontColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize ?? UIScale.width(5),
+        color: fontColor,
+      ),
+    );
+  }
+}
