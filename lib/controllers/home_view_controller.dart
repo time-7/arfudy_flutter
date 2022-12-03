2c8b015b42a8f52d39f8130ab0e05317cc1fce0c
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-12-03 12:45:54 -0300

    starting services

--- lib/controllers/home_view_controller.dart
+++ lib/controllers/home_view_controller.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
-class HomeViewController extends ChangeNotifier {}
+import '../models/meal_model.dart';
+
+enum HomeViewState { initial, loading, success, failure }
+
+class HomeViewController extends ChangeNotifier {
+  static final List<MealModel> _meals = [];
+  static final ValueNotifier<HomeViewState> _state = ValueNotifier(HomeViewState.initial);
+
+  static init() {
+    _state.value = HomeViewState.loading;
+    _state.notifyListeners();
+  }
+}
---
+++ lib/services/base_http_client.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+import '../models/meal_model.dart';
+import 'error/exceptions.dart';
+
+class BaseHttpClient {
+  static const String _baseUrl = "";
+
+  static Future<Map<String, dynamic>> getAsync<T>(String routeName) async {
+    try {
+      final String url = _baseUrl + routeName;
+      Response response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      final Map<String, dynamic> result = json.decode(response.body);
+      return result;
+    } catch (exception, stacktrace) {
+      throw GetBadRequisitionException(stacktrace, 'BadRequisition.GET', exception);
+    }
+  }
+}
+
+class CoreGateway {
+  static Future<List<MealModel>> getMeals() async {
+    try {
+      final Map<String, dynamic> result = await BaseHttpClient.getAsync("/xxxxx");
+      throw Exception(); //TODO NOT IMPLEMENTED
+    } catch (exception, stacktrace) {
+      throw GetMealsException(stacktrace, 'CoreGateway.getMeals', exception);
+    }
+  }
+}
