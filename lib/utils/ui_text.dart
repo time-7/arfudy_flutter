82025a6b2c06a05fa612c156983a304cbe21c257
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-12-04 19:17:53 -0300

    add new font

--- lib/utils/ui_text.dart
+++ lib/utils/ui_text.dart
@@ -4,13 +4,19 @@ import 'ui_scale.dart';
 
 class UIText extends StatelessWidget {
   const UIText(this.text,
-      {Key? key, this.fontSize, this.fontColor = Colors.black, this.textAlign, this.fontFamily = 'Antipasto'})
+      {Key? key,
+      this.fontSize,
+      this.fontColor = Colors.black,
+      this.textAlign,
+      this.fontFamily = 'Antipasto',
+      this.fontWeight})
       : super(key: key);
   final String text;
   final double? fontSize;
   final Color fontColor;
   final TextAlign? textAlign;
   final String fontFamily;
+  final FontWeight? fontWeight;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +27,7 @@ class UIText extends StatelessWidget {
         fontSize: fontSize ?? UIScale.width(5.7),
         color: fontColor,
         fontFamily: fontFamily,
+        fontWeight: fontWeight,
       ),
     );
   }
