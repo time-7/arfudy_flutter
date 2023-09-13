3f3e679d324b881dad12247ae9f0a8a8a54bac28
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2023-09-13 12:09:34 -0300

    create aqr code view file and its controller file

---
+++ lib/views/qr_code_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+
+import '../controllers/qr_code_view_controller.dart';
+
+class QrCodeView extends GetView<QrCodeViewController> {
+  const QrCodeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
