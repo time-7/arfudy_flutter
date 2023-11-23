8046e510709fe93b1868bdad432b854ecccab44a
Julia Stahl <julia.stahl@kepha.com.br>
2023-11-23 10:52:27 -0300

    Atualiza models para novo backend

--- lib/models/ingredient_model.dart
+++ lib/models/ingredient_model.dart
@@ -1,26 +1,51 @@
 import '../utils/proxier.dart';
+import 'nutrition_facts_model.dart';
 
 class IngredientModel {
-  final int id;
   final String name;
+  final int quantity;
+  final NutritionFactsModel nutritionFacts;
 
   IngredientModel({
-    required this.id,
     required this.name,
+    required this.quantity,
+    required this.nutritionFacts,
   });
 
   static IngredientModel generateProxySingle() => IngredientModel(
-        id: proxyInt,
         name: proxyRandomAmountWords(4),
+        quantity: proxyInt,
+        nutritionFacts: NutritionFactsModel.generateProxySingle(),
       );
 
-  static List<IngredientModel> generateProxyList({int lenght = 12}) => List.generate(
+  static List<IngredientModel> generateProxyList({int lenght = 12}) =>
+      List.generate(
         lenght,
         (index) => generateProxySingle(),
       );
 
-  factory IngredientModel.fromJson(Map<String, dynamic> json) => IngredientModel(
-        id: json["idIngrediente"],
-        name: json["nome"],
+  factory IngredientModel.fromJson(Map<String, dynamic> json) =>
+      IngredientModel(
+        name: json["name"],
+        quantity: json["quantity"],
+        nutritionFacts: NutritionFactsModel.fromJson(json["nutritionFacts"]),
       );
+
+  @override
+  String toString() {
+    return 'IngredientModel{name: $name, quantity: $quantity, nutritionFacts: $nutritionFacts}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is IngredientModel &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          quantity == other.quantity &&
+          nutritionFacts == other.nutritionFacts;
+
+  @override
+  int get hashCode =>
+      name.hashCode ^ quantity.hashCode ^ nutritionFacts.hashCode;
 }
--- lib/models/meal_model.dart
+++ lib/models/meal_model.dart
@@ -1,14 +1,17 @@
 import '../utils/proxier.dart';
 import 'ingredient_model.dart';
+import 'nutrition_facts_model.dart';
 
 class MealModel {
-  final int id;
+  final String id;
   final String name;
   final String description;
   final String imageUrl;
-  final bool has3d;
-  final String mealPrice;
-  final double nutritionalValue;
+  final bool isVisible;
+  final bool has3dModel;
+  final String? unityModelId;
+  final String price;
+  final NutritionFactsModel nutritionFacts;
   final List<IngredientModel> ingredients;
 
   MealModel({
@@ -16,20 +19,23 @@ class MealModel {
     required this.name,
     required this.description,
     required this.imageUrl,
-    required this.has3d,
-    required this.mealPrice,
-    required this.nutritionalValue,
+    required this.isVisible,
+    this.unityModelId,
+    required this.has3dModel,
+    required this.price,
+    required this.nutritionFacts,
     required this.ingredients,
   });
 
   static MealModel generateProxySingle() => MealModel(
-      id: proxyInt,
+      id: '123456789',
       name: proxyRandomAmountWords(8),
       description: proxyWords(20),
       imageUrl: proxyImgUrl,
-      has3d: proxyBool,
-      mealPrice: (proxyDouble * 100).toStringAsFixed(2),
-      nutritionalValue: proxyDouble,
+      has3dModel: proxyBool,
+      price: (proxyDouble * 100).toStringAsFixed(2),
+      isVisible: true,
+      nutritionFacts: NutritionFactsModel.generateProxySingle(),
       ingredients: IngredientModel.generateProxyList(lenght: 5));
 
   static List<MealModel> generateProxyList({int lenght = 12}) => List.generate(
@@ -38,12 +44,23 @@ class MealModel {
       );
 
   factory MealModel.fromJson(Map<String, dynamic> json) => MealModel(
-      id: json["idPrato"],
-      name: json["nome"],
-      description: json["dsPrato"],
-      imageUrl: json["imagem"],
-      has3d: json["has3d"],
-      mealPrice: json["valor"].toString(),
-      nutritionalValue: json["valorNutricional"],
-      ingredients: List<IngredientModel>.from(json["ingredienteList"].map((x) => IngredientModel.fromJson(x))));
+        id: json["id"].toString(),
+        name: json["name"].toString(),
+        description: json["description"].toString(),
+        imageUrl: json["imageUrl"].toString(),
+        has3dModel: json["has3dModel"],
+        price: json["price"].toString(),
+        nutritionFacts: NutritionFactsModel.fromJson(json["nutritionFacts"]),
+        isVisible: json["isVisible"],
+        ingredients: List<IngredientModel>.from(
+          json["ingredients"].map(
+            (x) => IngredientModel.fromJson(x),
+          ),
+        ),
+      );
+
+  @override
+  String toString() {
+    return 'MealModel{id: $id, name: $name, description: $description, imageUrl: $imageUrl, isVisible: $isVisible, has3dModel: $has3dModel, unityModelId: $unityModelId, price: $price, nutritionFacts: $nutritionFacts, ingredients: $ingredients}';
+  }
 }
